49ac09fd922c0b89974f3af17343120dbf3165bf
shweta <91779835+ShwetaPandey0407@users.noreply.github.com>
2023-10-25 19:21:35 +0530

    2st commit

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,3 +1,3 @@
 // export const baseURL = "http://localhost:5000/api"
 
-export const baseURL = "https://todo-app-backend-nn45.onrender.com"
+export const baseURL = "https://todo-app-backend-nn45.onrender.com/api"
